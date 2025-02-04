70087c53941d857002beb3e56e982b28d64b328d
anantariskys <gamersindo555@gmail.com>
2025-02-04 23:43:55 +0700

    Create CTA.tsx

---
+++ src/components/CTA.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CTAImage from "@/assets/cta-img.jpg";
+
+const CTA = () => {
+  return (
+    <section className="container py-16">
+      <main
+        className="w-full aspect-[16/6]  rounded-2xl  text-white flex items-center justify-center  bg-primary bg-cover bg-top bg-no-repeat overflow-hidden"
+        style={{ backgroundImage: `url(${CTAImage.src})` }}
+      >
+        <div className="size-full bg-primary bg-opacity-75"/>
+        <div className="absolute flex flex-col gap-8 items-center justify-center">
+          <p className="text-5xl font-semibold text-center max-w-4xl">
+            Bingung memulai trading? Konsultasikan dengan tim{" "}
+            <span className="text-[#25C660]">RajaFx</span>
+          </p>
+          <button className="bg-white text-primary py-2 px-8 rounded-lg">
+            Hubungi Kami
+          </button>
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default CTA;
