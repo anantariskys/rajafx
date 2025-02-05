79c11c0da448ea1240e7b79d3929669fc4a275f1
anantariskys <gamersindo555@gmail.com>
2025-02-05 22:07:52 +0700

    Create benefits.ts

---
+++ src/data/benefits.ts
@@ -0,0 +1,24 @@
+const benefits = [
+    {
+        title:'Akses eksklusif ke  broker andalan kami',
+        description: "Dapatkan akses eksklusif dengan broker pilihan kami dengan rekam jejak unggul yang menawarkan berbagai fitur terbaik untuk mendukung semua kebutuhan trading Anda",
+        icon:"icon-park-outline:trademark"
+    },
+    {
+        title:'Spread rendah dan leverage tinggi',
+        description: "Nikmati spread mulai dari 0.0 pip dan leverage hingga 1:2000 di broker pilihan kami",
+        icon:"hugeicons:trade-up"
+    },
+    {
+        title:'Bonus dan promo eksklusif',
+        description: "Dapatkan rebate untuk setiap transaksi dan promosi pendaftaran eksklusif yang meningkatkan keuntungan Anda",
+        icon:"mingcute:gift-fill"
+    },
+    {
+        title:'Dukungan layanan 24/7 ',
+        description: "Tim ahli kami siap membantu anda kapan saja, memastikan pengalaman trading Anda berjalan lancar",
+        icon:"tdesign:service-filled"
+    },
+]
+
+export default benefits
