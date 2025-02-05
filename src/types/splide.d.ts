8b921d61f8d8ccdf5592f451228b9df2a76a8582
anantariskys <gamersindo555@gmail.com>
2025-02-05 23:05:04 +0700

    Update splide.d.ts

--- src/types/splide.d.ts
+++ src/types/splide.d.ts
@@ -1,6 +1,6 @@
+/* eslint-disable */
 declare module '@splidejs/react-splide' {
     import { ComponentType } from 'react';
-  
     export const Splide: ComponentType<any>;
     export const SplideSlide: ComponentType<any>;
   }
