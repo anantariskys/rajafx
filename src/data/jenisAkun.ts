0121d912918526fc30fba367f79d4ad20b00aec3
anantariskys <gamersindo555@gmail.com>
2025-02-05 22:08:37 +0700

    Create jenisAkun.ts

---
+++ src/data/jenisAkun.ts
@@ -0,0 +1,73 @@
+import Standart from "@/assets/standart.png";
+import Shares from "@/assets/shares.png";
+import Ultralow from "@/assets/ultralow.png";
+
+export const jenisAkunXM = [
+  {
+    image: Standart,
+    name: "Standard",
+    description:
+      "Akun XM paling populer, nikmati bonus untuk meningkatkan kemampuan trading Anda, dan antisipasi potensi risiko terhadap modal Anda.",
+    minimum_deposit: "$5",
+  },
+  {
+    image: Ultralow,
+    name: "Ultra Low",
+    description:
+      "Solusi ideal bagi anda yang ingin trading dengan volume tinggi. Nikmati penghematan melalui spread, swap, dan komisi yang lebih rendah.",
+    minimum_deposit: "$5",
+  },
+  {
+    image: Shares,
+    name: "Shares",
+    description:
+      "Cocok untuk anda yang ingin berinvestasi saham dan manfaatkan beragam peluang di pasar saham.",
+    minimum_deposit: "$10,000",
+  },
+];
+
+export const jenisAkunValetax = [
+  {
+    icon: "majesticons:cent-circle",
+    name: "CENT",
+    description:
+      "Cocok untuk anda trader pemula dan algoritmik, memberikan kesempatan untuk mengalami strategi trading dengan risiko rendah.",
+    minimum_deposit: "$1",
+  },
+  {
+    icon: "fa6-solid:money-bill",
+    name: "Standard",
+    description:
+      "Cocok untuk trader yang mencari leverage hingga 1:2000, menawarkan kondisi trading yang mirip dengan pasar antar bank dan institusi.",
+    minimum_deposit: "$1",
+  },
+  {
+    icon: "teenyicons:star-circle-solid",
+
+    name: "ECN",
+    description:
+      "Cocok untuk trader ahli dan scalper, dengan spread nol dan akses langsung dari penyedia likuiditas serta.",
+    minimum_deposit: "$50",
+  },
+  {
+    icon: "fa6-solid:money-bill",
+    name: "Booster",
+    description:
+      "Akun Booster menawarkan keuntungan ganda dari trading pribadi dan referral, cocok untuk anda yang ingin meningkatkan pengalaman dan memperluas jaringan.",
+    minimum_deposit: "$1",
+  },
+  {
+    icon: "mingcute:gift-fill",
+    name: "Bonus",
+    description:
+      "Dapatkan keuntungan maksimal dengan bonus, yang memberikan fleksibilitas penuh dalam mengelola modal dan berbagai strategi trading.",
+    minimum_deposit: "$1",
+  },
+  {
+    icon: "fluent:crown-24-filled",
+    name: "PRO",
+    description:
+      "Akun Pro dirancang khusus untuk trader berpengalaman yang memerlukan kondisi trading terbaik dengan spread ultra-rendah, eksekusi tinggi, dan tanpa komisi.",
+    minimum_deposit: "$500",
+  },
+];
