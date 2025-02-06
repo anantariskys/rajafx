71283ef996cabeda58ca50839125681bc15622c1
anantariskys <gamersindo555@gmail.com>
2025-02-06 23:42:32 +0700

    Update SectionLayout.tsx

--- src/layouts/SectionLayout.tsx
+++ src/layouts/SectionLayout.tsx
@@ -1,11 +1,13 @@
 import SubTitleSection from "@/components/Section/SubTitleSection";
 import TitleSection from "@/components/Section/TitleSection";
+import { Icon } from "@iconify/react/dist/iconify.js";
 import  { FC, ReactNode } from "react";
 
 interface SectionLayoutProps {
   children: ReactNode;
   title?: ReactNode;
   subTitle?: ReactNode;
+  icon?: string;
   autoContainer?: boolean;
 }
 
@@ -13,11 +15,20 @@ const SectionLayout: FC<SectionLayoutProps> = ({
   children,
   subTitle,
   title,
-  autoContainer=true
+  autoContainer=true,
+  icon
 }) => {
   return (
-    <section className={`${autoContainer&&"container"} space-y-6`}>
+    <section className={`${autoContainer&&"container"} py-24 space-y-6`}>
       <div className="space-y-4">
+        {
+          icon&&(
+            <div className="p-4 bg-[#E8F1FD] text-2xl w-fit mx-auto  rounded-full text-[#09326C]">
+          <Icon icon={icon} />
+        </div>
+          )
+        }
+    
         {title && <TitleSection>{title}</TitleSection>}
         {subTitle && <SubTitleSection>{subTitle}</SubTitleSection>}
       </div>
