9eea7c8262d5852b3511cf87ecf5e4a1aa2eede8
anantariskys <gamersindo555@gmail.com>
2025-02-06 23:42:15 +0700

    Update CTA.tsx

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,13 +1,22 @@
+'use client'
 import React from "react";
 import CTAImage from "@/assets/cta-img.jpg";
+import { usePathname } from "next/navigation";
+import SectionLayout from "@/layouts/SectionLayout";
 
 const CTA = () => {
+   const pathname = usePathname(); 
   return (
-    <section className="container py-16">
+   <SectionLayout
+   title={pathname=='/xm'&&<>
+   Mulai Trading di <span className="text-[#1570EF]">XM</span>
+   </>}
+   >
+
       <main
         className="w-full aspect-[16/6]  rounded-2xl  text-white flex items-center justify-center  bg-primary bg-cover bg-top bg-no-repeat overflow-hidden"
         style={{ backgroundImage: `url(${CTAImage.src})` }}
-      >
+        >
         <div className="size-full bg-primary bg-opacity-75"/>
         <div className="absolute flex flex-col gap-8 items-center justify-center">
           <p className="text-5xl font-semibold text-center max-w-4xl">
@@ -19,7 +28,7 @@ const CTA = () => {
           </button>
         </div>
       </main>
-    </section>
+  </SectionLayout>
   );
 };
 
