36830357032079ea27c4dd59250263c9f9b3fbb0
anantariskys <gamersindo555@gmail.com>
2025-02-07 00:24:14 +0700

    Create page.tsx

---
+++ src/app/xm/jenis-akun/page.tsx
@@ -0,0 +1,15 @@
+import SectionLayout from '@/layouts/SectionLayout'
+import React from 'react'
+
+const JenisAkun = () => {
+  return (
+    <div className='pt-48'>
+  <SectionLayout title='Jenis Akun' icon='solar:user-bold' subTitle='Pilih jenis akun sesuai kebutuhan anda  yang terus berkembang.'>
+    <main className='flex items-center pt-10 justify-between'>
+    </main>
+  </SectionLayout>
+  </div>
+  )
+}
+
+export default JenisAkun
