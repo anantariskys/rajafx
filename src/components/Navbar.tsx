369d61835a3b477fb5b9d3fea2bcd9baf4f3972e
anantariskys <gamersindo555@gmail.com>
2025-02-06 23:42:18 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import Logo from '@/assets/logo.svg'
 import Image from "next/image";
 
 const Navbar = () => {
-  const pathname = usePathname(); // Dapatkan path halaman saat ini
+  const pathname = usePathname(); 
 
   const menuItems = [
     { name: "Home", path: "/" },
