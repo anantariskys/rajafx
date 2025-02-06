f8b2e41ed2458031d86039e5f2a12a2ca5088ea5
anantariskys <gamersindo555@gmail.com>
2025-02-06 23:42:29 +0700

    Create whyXM.ts

---
+++ src/data/whyXM.ts
@@ -0,0 +1,25 @@
+const whyXM = [
+    {
+      icon: "streamline:arrow-shrink-diagonal-2",
+      title: "Spread Ketat",
+      description: "Trading dengan spread mulai dari 0.8 pip",
+    },
+    {
+      icon: "lsicon:double-arrow-down-outline",
+      title: "Biaya Trading Rendah",
+      description: "Trading tanpa biaya swap dan komisi",
+    },
+    {
+      icon: "fluent-mdl2:reminder-time",
+      title: "Harga Langsung dari Pasar",
+      description: "Trading dengan harga yang adil tanpa requote dan penolakan order",
+    },
+    {
+      icon: "fluent:flash-16-filled",
+      title: "Penarikan Cepat",
+      description: "Dapatkan dana Anda dengan mudah tanpa potongan biaya",
+    },
+  ];
+  
+  export default whyXM;
+  
