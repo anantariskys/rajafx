0c17783b5ecf27310d1391949fd4afeac98ce074
anantariskys <gamersindo555@gmail.com>
2025-02-07 09:46:06 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,43 @@
-  "use client";
+"use client";
 
-  import Link from "next/link";
-  import { usePathname } from "next/navigation";
-  import Logo from '@/assets/logo.svg'
-  import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Logo from "@/assets/logo.svg";
+import Image from "next/image";
 import { useMemo } from "react";
 
-  const Navbar = () => {
-    const pathname = usePathname(); 
+const Navbar = () => {
+  const pathname = usePathname();
 
-
-    const menuItems = useMemo(
-      () => [
-        { name: "Home", path: "/", activePath: ["/"] },
-        { name: "Valetax", path: "/valetax", activePath: ["/valetax"] },
-        { name: "XM", path: "/xm", activePath: ["/xm", "/xm/copy-trading", "/xm/jenis-akun"] },
-        { name: "Perbandingan", path: "/perbandingan", activePath: ["/perbandingan"] },
-        { name: "Hubungi Kami", path: "/hubungi-kami", activePath: ["/hubungi-kami"] },
-      ],
-      []
-    );
-    return (
-      <nav className="bg-primary z-50 fixed top-0 w-full  text-[#9A9CA0]">
-        <main className="flex container py-6 justify-between items-center">
-          <div className="flex items-center  gap-2">
-            <Image src={Logo} alt="logo"/>
-            <h1 className="text-white text-2xl font-semibold">RAJAFX</h1>
-          </div>
+  const menuItems = useMemo(
+    () => [
+      { name: "Home", path: "/", activePath: ["/"] },
+      { name: "Valetax", path: "/valetax", activePath: ["/valetax"] },
+      {
+        name: "XM",
+        path: "/xm",
+        activePath: ["/xm", "/xm/copy-trading", "/xm/jenis-akun"],
+      },
+      {
+        name: "Perbandingan",
+        path: "/perbandingan",
+        activePath: ["/perbandingan"],
+      },
+      {
+        name: "Hubungi Kami",
+        path: "/hubungi-kami",
+        activePath: ["/hubungi-kami"],
+      },
+    ],
+    []
+  );
+  return (
+    <nav className="bg-primary z-50 fixed top-0 w-full  text-[#9A9CA0]">
+      <main className="flex container py-6 justify-between items-center">
+        <div className="flex items-center  gap-2">
+          <Image src={Logo} alt="logo" />
+          <h1 className="text-white text-2xl font-semibold">RAJAFX</h1>
+        </div>
         <ul className="flex gap-8">
           {menuItems.map(({ name, path, activePath }) => {
             const isActive = activePath.includes(pathname);
@@ -34,7 +45,9 @@ import { useMemo } from "react";
               <li key={path}>
                 <Link
                   href={path}
-                  className={`transition-all ${isActive ? "text-white font-semibold" : "hover:text-white"}`}
+                  className={`transition-all ${
+                    isActive ? "text-white font-semibold" : "hover:text-white"
+                  }`}
                   aria-current={isActive ? "page" : undefined}
                 >
                   {name}
@@ -43,9 +56,9 @@ import { useMemo } from "react";
             );
           })}
         </ul>
-        </main>
-      </nav>
-    );
-  };
+      </main>
+    </nav>
+  );
+};
 
-  export default Navbar;
+export default Navbar;
