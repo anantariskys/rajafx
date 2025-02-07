77263aed555655438562e4b52d1fe18473e675cb
anantariskys <gamersindo555@gmail.com>
2025-02-08 00:12:13 +0700

    Create CopyTrading.tsx

---
+++ src/components/Section/valetax/CopyTrading.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Img from '@/assets/valetax-account-example.png'
+import Image from 'next/image'
+import Button from '@/components/Button'
+import Link from 'next/link'
+const CopyTrading = () => {
+  return (
+    <section className='flex md:flex-row flex-col items-start py-24 gap-8'>
+        <Image src={Img} alt='copy trading' className='md:w-2/5 border-2 rounded-2xl'/>
+        <div className='flex flex-col gap-4 max-w-2xl'>
+            <h2 className='text-5xl font-semibold'>Salin Strategi Trader Profesional Secara Otomatis</h2>
+            <p className='text-[#6B6E73]'>Dapatkan keuntungan dari pengalaman trader profesional tanpa perlu menganalisis pasar sendiri. Dengan Copy Trading Valetax, Anda bisa menyalin strategi para ahli secara otomatis, memungkinkan Anda meraih profit dengan mudah tanpa harus menjadi trader berpengalaman.</p>
+            <Link href={'valetax/copy-trading'}>
+            <Button>Selengkapnya</Button>
+            </Link>
+        </div>
+    </section>
+  )
+}
+
+export default CopyTrading
