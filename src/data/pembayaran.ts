42916be5795c38a029c08a0ce8ad95e89439ff6a
anantariskys <gamersindo555@gmail.com>
2025-02-08 00:12:36 +0700

    Create pembayaran.ts

---
+++ src/data/pembayaran.ts
@@ -0,0 +1,21 @@
+import IMG1 from '@/assets/bri.png'
+import IMG2 from '@/assets/bca.png'
+import IMG3 from '@/assets/cimb.png'
+import IMG4 from '@/assets/mandiri.png'
+import IMG5 from '@/assets/bni.png'
+import IMG6 from '@/assets/qris.png'
+import IMG7 from '@/assets/danamon.png'
+import IMG8 from '@/assets/permata.png'
+
+const pembayaran = [
+    IMG1,
+    IMG2,
+    IMG3,
+    IMG4,    
+    IMG5,
+    IMG6,
+    IMG7,
+    IMG8
+]
+
+export default pembayaran
