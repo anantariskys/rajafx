b09afd65d860f7afd47f5d087be72c02d96883cd
anantariskys <gamersindo555@gmail.com>
2025-02-08 00:12:34 +0700

    Create layanan.ts

---
+++ src/data/layanan.ts
@@ -0,0 +1,37 @@
+import Layanan1 from '@/assets/layanan-valetax1.png'
+import Layanan2 from '@/assets/layanan-valetax2.png'
+import Layanan3 from '@/assets/layanan-valetax3.png'
+import Layanan4 from '@/assets/layanan-valetax4.png'
+
+
+export const layananValetax = [
+    {
+      id: 1,
+      title: "Buat Akun",
+      description:
+        "Mulai perjalanan trading anda dari sini. Buka akun dengan 3 langkah mudah",
+      image: Layanan1,
+    },
+    {
+      id: 2,
+      title: "Klaim Rebate",
+      description:
+        "Dapatkan cashback otomatis hingga 75% dari setiap transaksi untuk memaksimalkan profit anda",
+      image: Layanan2,
+    },
+    {
+      id: 3,
+      title: "Withdrawal",
+      description:
+        "Nikmati kemudahan withdrawal dengan berbagai metode pembayaran lokal yang aman dan efisien",
+      image: Layanan3,
+    },
+    {
+      id: 4,
+      title: "Deposit",
+      description:
+        "Nikmati kemudahan deposit dengan berbagai metode pembayaran lokal yang aman dan efisien",
+      image: Layanan4,
+    },
+  ];
+  
