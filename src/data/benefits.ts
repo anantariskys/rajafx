7c7addf6345a44ba8c48fae496de61a73a369092
anantariskys <gamersindo555@gmail.com>
2025-02-08 00:12:29 +0700

    Update benefits.ts

--- src/data/benefits.ts
+++ src/data/benefits.ts
@@ -1,4 +1,4 @@
-const benefits = [
+export const benefits = [
     {
         title:'Akses eksklusif ke  broker andalan kami',
         description: "Dapatkan akses eksklusif dengan broker pilihan kami dengan rekam jejak unggul yang menawarkan berbagai fitur terbaik untuk mendukung semua kebutuhan trading Anda",
@@ -21,4 +21,63 @@ const benefits = [
     },
 ]
 
-export default benefits
+export const valetaxBenefits =[
+    {
+      id: 1,
+      title: "Lokal Deposit dan Withdrawal",
+      description:
+        "Nikmati kemudahan transaksi deposit dan withdrawal dengan metode pembayaran lokal dengan cepat, mudah, dan tanpa hambatan finansial",
+      icon: "raphael:dollar",
+    },
+    {
+      id: 2,
+      title: "Share Rebate 75%",
+      description:
+        "Dapatkan cashback otomatis hingga 75% dari setiap transaksi untuk memaksimalkan profit anda",
+      icon: "flowbite:cash-outline",
+    },
+    {
+      id: 3,
+      title: "Free join Copytrading",
+      description:
+        "Gabung dengan Copy Trading Valetax dan tingkatkan potensi profit Anda dengan meniru strategi trader profesional secara otomatis, tanpa perlu pengalaman atau analisis rumit",
+      icon: "hugeicons:trade-up",
+    },
+    {
+      id: 4,
+      title: "Free Merchandise",
+      description:
+        "Cukup dengan deposit $150 USD, anda akan mendapatkan merchandise yang menarik dan khusus dibuat untuk klien RajaFX di bawah afiliasi kami",
+      icon: "mingcute:gift-fill",
+    },
+  ];
+
+
+
+  export const valetaxCopyTradingBenefits =[
+    {
+      "title": "Pantau dan lacak",
+      "description": "Lacak dan sesuaikan pengaturan Anda dengan kontrol penuh atas akun Anda.",
+      "icon": "hugeicons:trade-up"
+    },
+    {
+      "title": "Investasi yang Diversifikasi",
+      "description": "Diversifikasi risiko secara lancar dengan beragam strategi.",
+      "icon": "mdi:dots-triangle"
+    },
+    {
+      "title": "Otomatis",
+      "description": "Rasakan ketenangan pikiran yang didapat dari perdagangan otomatis.",
+      "icon": "weui:setting-filled"
+    },
+    {
+      "title": "Kelola Risiko",
+      "description": "Tetapkan tingkat risiko yang paling sesuai untuk Anda.",
+      "icon": "fluent:flash-16-filled"
+    },
+    {
+      "title": "Perdagangan yang Mudah",
+      "description": "Salin perdagangan para ahli dengan mudah.",
+      "icon": "teenyicons:adjust-vertical-alt-solid"
+    }
+  ]
