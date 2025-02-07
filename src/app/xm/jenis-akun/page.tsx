4073763389d1ba568dc039feb7c02768c84ba960
anantariskys <gamersindo555@gmail.com>
2025-02-07 09:45:46 +0700

    Update page.tsx

--- src/app/xm/jenis-akun/page.tsx
+++ src/app/xm/jenis-akun/page.tsx
@@ -1,15 +1,24 @@
-import SectionLayout from '@/layouts/SectionLayout'
-import React from 'react'
+import JenisAkunCard from "@/components/JenisAkunCard";
+import { jenisAkunXM } from "@/data/jenisAkun";
+import SectionLayout from "@/layouts/SectionLayout";
+import React from "react";
 
 const JenisAkun = () => {
   return (
-    <div className='pt-48'>
-  <SectionLayout title='Jenis Akun' icon='solar:user-bold' subTitle='Pilih jenis akun sesuai kebutuhan anda  yang terus berkembang.'>
-    <main className='flex items-center pt-10 justify-between'>
-    </main>
-  </SectionLayout>
-  </div>
-  )
-}
+    <div className="pt-48">
+      <SectionLayout
+        title="Jenis Akun"
+        icon="solar:user-bold"
+        subTitle="Pilih jenis akun sesuai kebutuhan anda  yang terus berkembang."
+      >
+        <main className="flex max-w-5xl mx-auto gap-4 justify-center">
+          {jenisAkunXM.map((item, index) => (
+            <JenisAkunCard link="" linkText="Buka akun" item={item} key={index} />
+          ))}
+        </main>
+      </SectionLayout>
+    </div>
+  );
+};
 
-export default JenisAkun
+export default JenisAkun;
