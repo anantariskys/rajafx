41fb79e7b93b6970c7255cec0a905951e0ac2a5f
anantariskys <gamersindo555@gmail.com>
2025-02-08 00:12:46 +0700

    Update TitleSection.tsx

--- src/components/Section/TitleSection.tsx
+++ src/components/Section/TitleSection.tsx
@@ -4,7 +4,7 @@ interface Props {
   children: React.ReactNode;
 }
 const TitleSection: FC<Props> = ({ children }) => {
-  return <h3 className="text-center text-5xl font-semibold">{children}</h3>;
+  return <h2 className="text-center text-5xl font-semibold">{children}</h2>;
 };
 
 export default TitleSection;
