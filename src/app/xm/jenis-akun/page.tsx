afbe56f31d41003afadbb0756ec0fa8d44a5b056
anantariskys <gamersindo555@gmail.com>
2025-02-08 00:11:28 +0700

    Update page.tsx

--- src/app/xm/jenis-akun/page.tsx
+++ src/app/xm/jenis-akun/page.tsx
@@ -13,7 +13,7 @@ const JenisAkun = () => {
       >
         <main className="flex max-w-5xl mx-auto gap-4 justify-center">
           {jenisAkunXM.map((item, index) => (
-            <JenisAkunCard link="" linkText="Buka akun" item={item} key={index} />
+            <JenisAkunCard link="" linkText="Buka akun" item={item} key={index} additionalInfo={item} />
           ))}
         </main>
       </SectionLayout>
