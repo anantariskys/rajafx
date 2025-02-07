883c35d2c899768e366121c5cc79f1a30b258394
anantariskys <gamersindo555@gmail.com>
2025-02-08 00:12:31 +0700

    Create how.ts

---
+++ src/data/how.ts
@@ -0,0 +1,20 @@
+const how = [
+    {
+      "number": 1,
+      "title": "Pilih Money Manager",
+      "description": "Pilih Money Manager pilihan Anda berdasarkan tingkat risiko, profitabilitas, dan pengalamannya."
+    },
+    {
+      "number": 2,
+      "title": "Dana Setoran",
+      "description": "Danai akun yang ingin anda gunakan untuk CopyTrading."
+    },
+    {
+      "number": 3,
+      "title": "Mulai Copy Trading",
+      "description": "Akun berhasil terhubung ke money manager pilihan dan mulai copy trading sekarang juga."
+    }
+  ]
+
+
+  export default how
