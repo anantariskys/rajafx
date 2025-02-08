17589e6b9b93c8734de27535d6a57cb9a00046d2
anantariskys <gamersindo555@gmail.com>
2025-02-08 14:01:25 +0700

    Update About.tsx

--- src/components/Section/valetax/About.tsx
+++ src/components/Section/valetax/About.tsx
@@ -4,9 +4,9 @@ import Image from "next/image"
 const About = () => {
   return (
     <SectionLayout title={<>Berkenalan dengan <span className='text-[#25C660]'>Valetax</span></>}>
-    <main className='flex items-center justify-between max-w-6xl  gap-6 mx-auto'>
+    <main className='flex md:flex-row flex-col-reverse items-center justify-between max-w-6xl  gap-6 mx-auto'>
         <p className="text-[#6B6E73]">Valetax adalah broker forex dan CFD yang menawarkan solusi trading modern dengan berbagai keunggulan. Valetax menyediakan akses ke pasar global dengan spread kompetitif dan eksekusi order cepat, memastikan pengalaman trading yang efisien. Valetax juga menawarkan platform trading yang user-friendly, cocok untuk anda trader pemula maupun berpengalaman.</p>
-        <Image alt="about-img" className="max-w-md rounded-lg" src={AboutIMG}/>
+        <Image alt="about-img" className="md:max-w-md max-w-xs rounded-lg" src={AboutIMG}/>
      
 
     </main>
