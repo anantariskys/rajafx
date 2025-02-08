d4a98361008b351be9a31eed266ad228d1de1b11
anantariskys <gamersindo555@gmail.com>
2025-02-08 14:00:46 +0700

    Update page.tsx

--- src/app/hubungi-kami/page.tsx
+++ src/app/hubungi-kami/page.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image'
 import { Icon } from '@iconify/react/dist/iconify.js'
 const Hubungi = () => {
   return (
-    <div className="pt-48">
+    <div className="md:pt-48 pt-24">
     <SectionLayout
       title="Hubungi kami"
       subTitle="Tim ahli kami siap melayani dan memberikan pengalaman berharga untuk investasi anda"
       >
-        <main className='w-full flex bg-[#FAFAFA] rounded-xl p-4 justify-between items-end'>
+        <main className='w-full flex flex-col-reverse md:flex-row bg-[#FAFAFA] rounded-xl p-4 items-center justify-between md:items-end'>
             <div className='max-w-3xl border space-y-6 w-full p-4 bg-white rounded-xl'>
                 <p className='text-[#11592B] bg-[#E6F7E8] py-2 px-4 w-fit rounded-lg font-semibold '>RajaFX Customer Service</p>
                 <div className='space-y-2 text-lg font-semibold'>
@@ -30,7 +30,7 @@ const Hubungi = () => {
 
                 </div>
             </div>
-            <Image src={HubungiIMG} className='max-w-md' alt='hubungiImg' draggable={false}/>
+            <Image src={HubungiIMG} className='md:max-w-md max-w-xs' alt='hubungiImg' draggable={false}/>
 
         </main>
    
