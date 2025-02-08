3372927e7073a956391e11c5dfa0d00eecb8e6ed
anantariskys <gamersindo555@gmail.com>
2025-02-08 14:01:03 +0700

    Update CTA.tsx

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -14,14 +14,13 @@ const CTA = () => {
    Mulai Trading di <span className="text-[#1570EF]">XM</span>
    </>}
    >
-
       <main
-        className="w-full aspect-[16/6]  rounded-2xl  text-white flex items-center justify-center  bg-primary bg-cover bg-top bg-no-repeat overflow-hidden"
+        className="w-full md:aspect-[16/6] aspect-[16/8]    rounded-2xl relative  text-white flex items-center justify-center  bg-primary bg-cover bg-top bg-no-repeat overflow-hidden"
         style={{ backgroundImage: `url(${CTAImage.src})` }}
         >
         <div className="size-full bg-primary bg-opacity-75"/>
-        <div className="absolute flex flex-col gap-8 items-center justify-center">
-          <p className="text-5xl font-semibold text-center max-w-4xl">
+        <div className="absolute flex w-full  flex-col md:gap-8 gap-2 items-center justify-center">
+          <p className="md:text-5xl text-xl font-semibold text-center md:max-w-4xl">
             Bingung memulai trading? Konsultasikan dengan tim{" "}
             <span className="text-[#25C660]">RajaFx</span>
           </p>
