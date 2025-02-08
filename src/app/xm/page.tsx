2bcf66a8ca016d5e245d37f84ff063bb36722871
anantariskys <gamersindo555@gmail.com>
2025-02-08 14:02:28 +0700

    Update page.tsx

--- src/app/xm/page.tsx
+++ src/app/xm/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 import React from "react";
-import SectionLayout from "@/layouts/SectionLayout";
-import whyXM from "@/data/whyXM";
-import WhyCard from "@/components/XM/WhyCard";
-import { jenisAkunXM } from "@/data/jenisAkun";
-import JenisAkunCard from "@/components/JenisAkunCard";
 import Hero from "@/components/Section/xm/Hero";
 import About from "@/components/Section/xm/About";
 import Feature from "@/components/Section/xm/Feature";
