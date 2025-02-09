7ecd6abc012917aca396d07ca1e808cc82930e96
anantariskys <gamersindo555@gmail.com>
2025-02-09 23:23:09 +0700

    Update page.tsx

--- src/app/valetax/jenis-akun/[slug]/page.tsx
+++ src/app/valetax/jenis-akun/[slug]/page.tsx
@@ -1,70 +1,26 @@
-'use client'
-import Button from "@/components/Button";
-import DetailAkunCarousel from "@/components/valetax/DetailAkunCarousel";
 import { jenisAkunValetax2 } from "@/data/jenisAkun";
-import SectionLayout from "@/layouts/SectionLayout";
-import Image, { StaticImageData } from "next/image";
-import { useParams } from "next/navigation";
+import { Metadata } from "next";
 
-import { useEffect, useState } from "react";
+import DetailAkun from "./DetailAkun";
 
-interface AkunDetails {
-  "Deposit Minimal": string;
-  "Mata Uang Pasar": string;
-  "Ukuran Kontrak": string;
-  Spread: string;
-  Komisi: string;
-  "Leverage Maksimal": string;
-  "Harga Call/Stop Out": string;
-  "Instrumen Perdagangan": string;
+interface Props {
+  params: { slug: string };
 }
 
-interface Akun {
-  id: number;
-  heroIMG: StaticImageData;
-  name: string;
-  description: string;
-  description2: string;
-  description3: string;
-  spread: string;
-  icon: string;
-  details: AkunDetails;
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = params;
+  const akun = jenisAkunValetax2.find((item) => item.slug === slug);
+
+  return {
+    title: akun
+      ? `Akun ${akun.name2} | RajaFx`
+      : "Akun Tidak Ditemukan | RajaFx",
+    description: akun ? akun.description3 : "Halaman akun tidak ditemukan.",
+  };
 }
 
-const DetailAkun = () => {
-  const [data,setData] =useState<Akun>()
-  const {slug} = useParams();
-  
-
-  useEffect(() => {
-    if (slug) {
-      const filteredData = jenisAkunValetax2.find((item) => item.slug === slug);
-      setData(filteredData);
-    }
-  }, [slug]);
-
-  return (
-    <>
-      <section className="min-h-screen flex items-center">
-        <main className="container flex md:flex-row flex-col items-center  gap-16">
-          {data?.heroIMG&&(
-            <Image src={data?.heroIMG} alt={data?.name} className="md:max-w-md max-w-xs" draggable={false}/>
-          )}
-          <div className="max-w-2xl space-y-4">
-            <h1 className="text-5xl font-semibold">Akun {data?.name}</h1>
-            <p className="text-[#6B6E73]">{data?.description3}</p>
-            <Button>Buat Akun</Button>
-          </div>
-
-
-        </main>
-
-      </section>
-      <SectionLayout>
-      <DetailAkunCarousel/>
-      </SectionLayout>
-    </>
-  );
+const Page = () => {
+  return <DetailAkun />;
 };
 
-export default DetailAkun;
+export default Page;
