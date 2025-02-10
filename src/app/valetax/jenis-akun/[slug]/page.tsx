10190592447c82437ffacdfc68903a0116cc7858
anantariskys <gamersindo555@gmail.com>
2025-02-10 14:40:44 +0700

    Update page.tsx

--- src/app/valetax/jenis-akun/[slug]/page.tsx
+++ src/app/valetax/jenis-akun/[slug]/page.tsx
@@ -1,28 +1,85 @@
+'use client'
 import { jenisAkunValetax2 } from "@/data/jenisAkun";
 import { Metadata } from "next";
-
-import DetailAkun from "./DetailAkun";
+import Image, { StaticImageData } from "next/image";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import Button from "@/components/Button";
+import SectionLayout from "@/layouts/SectionLayout";
+import InfoAkun from "@/components/Section/valetax/jenis-akun/InfoAkun";
+import DetailAkunCarousel from "@/components/valetax/DetailAkunCarousel";
 
 interface Props {
   params: Promise<{ slug: string }>;
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-
-  const { slug } = await params;
-  const akun = jenisAkunValetax2.find((item) => item.slug === slug);
-
-  return {
-    title: akun
-      ? `Akun ${akun.name2} | RajaFx`
-      : "Akun Tidak Ditemukan | RajaFx",
-    description: akun ? akun.description3 : "Halaman akun tidak ditemukan.",
-  };
+interface AkunDetails {
+  "Deposit Minimal": string;
+  "Mata Uang Pasar": string;
+  "Ukuran Kontrak": string;
+  Spread: string;
+  Komisi: string;
+  "Leverage Maksimal": string;
+  "Harga Call/Stop Out": string;
+  "Instrumen Perdagangan": string;
 }
 
-const Page = async ({ params }: Props) => {
-  // const { slug } = await params;
-  return <DetailAkun />;
+interface Akun {
+  id: number;
+  heroIMG: StaticImageData;
+  name: string;
+  name2: string;
+  description: string;
+  description2: string;
+  description3: string;
+  spread: string;
+  icon: string;
+  details: AkunDetails;
+}
+
+
+const Page = ()=> {
+  const [data, setData] = useState<Akun>();
+  const { slug } = useParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (slug) {
+      const filteredData = jenisAkunValetax2.find((item) => item.slug === slug);
+      if (!filteredData) {
+        router.back()
+      }
+      setData(filteredData);
+    }
+    window.scrollTo(0, 0);
+  }, [slug]);
+  
+  return (
+    <>
+    <section className="min-h-screen flex items-center">
+    <main className="container flex md:flex-row flex-col items-center  gap-16">
+      {data?.heroIMG && (
+        <Image
+          src={data?.heroIMG}
+          alt={data?.name}
+          className="md:max-w-md max-w-xs"
+          draggable={false}
+        />
+      )}
+      <div className="max-w-2xl space-y-4">
+        <h1 className="text-5xl font-semibold">Akun {data?.name2}</h1>
+        <p className="text-[#6B6E73] text-lg">{data?.description3}</p>
+        <Button>Buat Akun</Button>
+      </div>
+    </main>
+  </section>
+  {data?.details && <InfoAkun item={data.details} />}
+  <SectionLayout>
+    <h3 className="text-3xl md:text-5xl font-semibold text-center">Apa perbedaannya?</h3>
+    <DetailAkunCarousel />
+  </SectionLayout>
+    </>
+  );
 };
 
 export default Page;
