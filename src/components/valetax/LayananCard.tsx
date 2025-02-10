5aa6828623f96d5ed8be7fa1768893da11a78b82
anantariskys <gamersindo555@gmail.com>
2025-02-10 14:25:37 +0700

    Update LayananCard.tsx

--- src/components/valetax/LayananCard.tsx
+++ src/components/valetax/LayananCard.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 
 interface LayananCardProps {
@@ -6,15 +7,19 @@ interface LayananCardProps {
         image:StaticImageData,
         title:string,
         description:string
-    }
+        
+    },
+    link:string
 }
-const LayananCard:FC<LayananCardProps> = ({item}) => {
+const LayananCard:FC<LayananCardProps> = ({item,link}) => {
   return (
+    <Link href={link}>
     <div className="w-full hover:shadow-lg duration-300 ease-in-out p-6 border rounded-3xl">
         <Image alt={item.title} className="w-32" src={item.image}/>
       <h3 className="text-2xl font-semibold">{item.title}</h3>
       <p className="text-sm font-light">{item.description}</p>
     </div>
+    </Link>
   );
 };
 
