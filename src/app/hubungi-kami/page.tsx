35794db807f02183804300e9b1ca1c63f3608796
anantariskys <gamersindo555@gmail.com>
2025-02-11 20:34:56 +0700

    Update page.tsx

--- src/app/hubungi-kami/page.tsx
+++ src/app/hubungi-kami/page.tsx
@@ -16,12 +16,12 @@ const Hubungi = () => {
                 <div className='space-y-2 text-lg font-semibold'>
                     <div className='flex items-center gap-4'>
                         <Icon className='text-3xl' icon={'ic:baseline-whatsapp'}/>
-                        <p>0812-3456-7890</p>   
+                        <p>0856-4641-6605</p>   
                     </div>
 
                     <div className='flex items-center gap-4'>
                         <Icon className='text-3xl' icon={'ic:outline-email'}/>
-                        <p>emailcontoh@gmail.com</p>   
+                        <p>rajafxcs@gmail.com</p>   
                     </div>
                     <div className='flex items-center gap-4'>
                         <Icon className='text-3xl' icon={'majesticons:clock-line'}/>
