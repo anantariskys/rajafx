2daf1a5bc4a74781c914a9f0a2c223d520358f0a
anantariskys <gamersindo555@gmail.com>
2025-02-11 20:35:24 +0700

    Create rebate.ts

---
+++ src/data/rebate.ts
@@ -0,0 +1,29 @@
+const rebate = [
+    {
+      "title": "Rebate hingga 75%",
+      "description": "Dapatkan kembali sebagian besar biaya trading Anda hingga 75%",
+      "icon": "lsicon:badge-promotion-filled"
+    },
+    {
+      "title": "Pembayaran Fleksibel",
+      "description": "Rebate akan dibagikan setiap hari senin dan bisa ditransfer ke bank sesuai request klien",
+      "icon": "formkit:dollar"
+    },
+    {
+      "title": "Berlaku Untuk Semua Akun",
+      "description": "Rebate dapat diterapkan di segala tipe akun trading.",
+      "icon": "material-symbols:check-circle-outline-rounded"
+    },
+    {
+      "title": "Rebate 100% Tanpa Batasan",
+      "description": "Loss, profit, atau margin call (MC) tetap mendapatkan rebate penuh.",
+      "icon": "hugeicons:trade-up"
+    },
+    {
+      "title": "Profit dari Setiap Transaksi",
+      "description": "Bergabung sekarang dan dapatkan keuntungan tetap dari setiap aktivitas trading Anda",
+      "icon": "mingcute:gift-fill"
+    }
+  ]
+
+  export default rebate
